403c8b0196d91846a2acf678ef814010daa16127
zyh704 <elithan@163.com>
2018-07-31 23:40:37 +0800

    Add files via upload
    add..

---
+++ elithanJpa/src/main/java/com/elithan/elithanJpa/dao/Person.java
@@ -0,0 +1,51 @@
+package com.elithan.elithanJpa.dao;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private String age;
+
+	private Person(int id, String name, String age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	private Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
